b93db9fb720d9b53531d0a62f3dd06e7310a0755
Anurag018 <110323259+Anurag018@users.noreply.github.com>
2023-04-10 10:17:23 +0530

    Add files via upload

---
+++ CB.EN.U4CYS22069/even_odd.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+int n;
+printf("Check whether the number is even or odd:");
+scanf("%d,&n:");  //if the number is even the  output is 1
+ //if the number is odd the output is 0
+scanf("%d,n%2==0:");
+}
---
+++ CB.EN.U4CYS22069/sum_of_n_terms.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void) {
+int n,sum;
+printf("Enter the value:");
+scanf("%d",&n);
+sum=((n*(n+1))/2);
+
+printf("the sum of n terms %d",sum);
+}
